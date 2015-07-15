8ecec188df564856fcba7a51215e8aa930152fe7
Vishal Banthia <vishal.banthia@dena.com>
2015-07-15 11:52:37 +0900

    refactor

--- app/src/main/java/com/swet/buggyapp/RootActivity.java
+++ app/src/main/java/com/swet/buggyapp/RootActivity.java
@@ -1,38 +1,28 @@
 package com.swet.buggyapp;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
+import com.swet.buggyapp.bug.ButtonBugActivity;
 
-public class RootActivity extends ActionBarActivity {
+public class RootActivity extends Activity {
+
+    private Button buttonBugActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_root);
+
+        buttonBugActivity = (Button) findViewById(R.id.button_bug_activity_button);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_root, menu);
-        return true;
+    public void sendToButtonBugActivity(View _) {
+        Intent buttonBugActivityIntent = new Intent(this, ButtonBugActivity.class);
+        startActivity(buttonBugActivityIntent);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
---
+++ app/src/main/java/com/swet/buggyapp/bug/ButtonBugActivity.java
@@ -0,0 +1,45 @@
+package com.swet.buggyapp.bug;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.swet.buggyapp.R;
+
+public class ButtonBugActivity extends AbstractBugActivity {
+
+    private Button sayHi;
+    private Button sayHello;
+    private Button crashMe;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_button_bug);
+
+        sayHi    = (Button) findViewById(R.id.say_hi_button);
+        sayHello = (Button) findViewById(R.id.say_hello_button);
+        crashMe  = (Button) findViewById(R.id.crash_me_button);
+        textView = (TextView) findViewById(R.id.text_view);
+    }
+
+    public void sayHi(View _) {
+        this.textView.setText("Hii");
+    }
+
+    public void sayHello(View _) {
+        this.textView.setText("Hello");
+    }
+
+    public void crashMe(View _) {
+        // On pressing this button, application will be crashed for android 4.2.x
+        if (Build.VERSION.SDK_INT == 17) {
+            throw new RuntimeException("This is a crash");
+        }
+    }
+
+
+}
